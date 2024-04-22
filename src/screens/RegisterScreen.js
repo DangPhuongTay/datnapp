b6a9fed78de33a62acb86bbf92c1c58fdf1b3729
PhuongThaoCTK44 <2011442@dlu.edu.vn>
2024-04-22 10:42:34 +0700

    update fix start

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,44 +1,44 @@
-import React, {useContext, useState } from "react";
-import { Text, TextInput, View, TouchableOpacity, Button,StyleSheet,Image } from "react-native";
-import {AuthContext} from '../context/AuthContext';
+import React, { useContext, useState } from "react";
+import { Text, TextInput, View, TouchableOpacity, Button, StyleSheet, Image } from "react-native";
+import { AuthContext } from '../context/AuthContext';
 import Spinner from "react-native-loading-spinner-overlay";
 
-const RegisterSreen = ({navigation}) => {
-    const [name, setName ] = useState(null);
-    const [email, setEmail ] = useState(null);
-    const [password, setPassword ] = useState(null);
+const RegisterSreen = ({ navigation }) => {
+    const [name, setName] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
 
-    const {isLoading, register} = useContext(AuthContext);
+    const { isLoading, register } = useContext(AuthContext);
 
     return (
         <View style={styles.container}>
-             
-            <Spinner visible={isLoading}/>
-            <Image source={require('../../assets/images/register_img.png')} style={styles.img}></Image>
+
+            <Spinner visible={isLoading} />
+            <Image source={require('../../assets/images/item4_home_list.png')} style={styles.img}></Image>
             <TextInput placeholder="Tên"
-                    onChangeText={text => setName(text)}
-                    value={name}
-                    style={styles.input}/>
+                onChangeText={text => setName(text)}
+                value={name}
+                style={styles.input} />
             <TextInput placeholder="Email"
-                    onChangeText={text => setEmail(text)}
-                    value={email}
-                    style={styles.input}/>
+                onChangeText={text => setEmail(text)}
+                value={email}
+                style={styles.input} />
             <TextInput placeholder="Mật khẩu"
-                    onChangeText={text => setPassword(text)}
-                    value={password}
-                    style={styles.input}/>
+                onChangeText={text => setPassword(text)}
+                value={password}
+                style={styles.input} />
             <View>
                 <TouchableOpacity onPress={() => navigation.navigate('Login')}>
                     <Text style={styles.link}>Đã có tài khoản?</Text>
                 </TouchableOpacity>
             </View>
-            <TouchableOpacity style={styles.button} title="Đăng ký" 
-                    onPress={() => {
+            <TouchableOpacity style={styles.button} title="Đăng ký"
+                onPress={() => {
                     register(name, email, password);
                 }}><Text style={styles.btn}>Đăng ký</Text></TouchableOpacity>
-                
-            
-            
+
+
+
         </View>
     );
 };
@@ -50,46 +50,46 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         gap: 10,
     },
-   
-    input:{
-        
+
+    input: {
+
         width: 300,
         backgroundColor: '#eee',
-        borderRadius:999,
-        borderColor:'#62C7F3',
-        borderWidth:1,
-        paddingHorizontal:20,
+        borderRadius: 999,
+        borderColor: '#62C7F3',
+        borderWidth: 1,
+        paddingHorizontal: 20,
         paddingVertical: 10,
     },
-    button:{
+    button: {
         paddingVertical: 10,
         width: 220,
         marginBottom: 5,
-        backgroundColor:'#62C7F3',
-        display:'flex',
+        backgroundColor: '#62C7F3',
+        display: 'flex',
         alignItems: 'center',
-        borderRadius:999,
-        
+        borderRadius: 999,
+
     },
     link: {
         color: '#000',
-        textDecorationLine:'underline',
-        marginStart:170,
-        marginBottom:20
-        
+        textDecorationLine: 'underline',
+        marginStart: 170,
+        marginBottom: 20
+
     },
-    btn:{
-        color:'#fff',
-        fontSize:24,
-        fontWeight:'bold'
-       
+    btn: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold'
+
     },
-    img:{
-        width:150,
-        height:150,
-        objectFit:'contain',
-        marginBottom:10
+    img: {
+        width: 200,
+        height: 200,
+        objectFit: 'contain',
+        marginBottom: 10
     },
-    
+
 });
 export default RegisterSreen;
