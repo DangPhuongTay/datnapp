e5da0dc322f723fa7e685a7b1381b8858055b7d0
DangPhuongTay <89719935+DangPhuongTay@users.noreply.github.com>
2024-05-08 16:26:45 +0700

    update app

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,19 +3,61 @@ import { Text, View, ActivityIndicator, Button, TouchableOpacity, StyleSheet, Im
 import Spinner from "react-native-loading-spinner-overlay";
 import { AuthContext } from '../context/AuthContext';
 import { BASE_URL } from "../config";
-import medal1 from '../../assets/images/item6_home_list.png';
-import medal2 from '../../assets/images/item7_home_list.png';
-import medal3 from '../../assets/images/item8_home_list.png';
-import medal4 from '../../assets/images/item9_home_list.png';
+import img1 from '../../assets/images/category-1.png';
+import img2 from '../../assets/images/category-2.png';
+import img3 from '../../assets/images/category-3.png';
+import img4 from '../../assets/images/category-4.png';
+import img5 from '../../assets/images/category-5.png';
+import img6 from '../../assets/images/category-6.png';
+import img7 from '../../assets/images/category-7.png';
+import imgback from '../../assets/images/img_back.png';
+import imgnof from '../../assets/images/img-nof.png';
+import imgnotitem from '../../assets/images/img-not-item-1.gif';
+import imgnotitembg from '../../assets/images/img-not-item-bg.png';
+import imgbg1 from '../../assets/images/img-not-item-bg-1.png';
+import imgbg from '../../assets/images/img-bg-1.png';
+import imgcatwhite from '../../assets/images/img-cat-white-1.gif';
+import imgRectangle6 from '../../assets/images/Rectangle6.png';
+import imgRectangle5 from '../../assets/images/Rectangle5.png';
+import imgRectangle2 from '../../assets/images/Rectangle2.png';
+import imgvectorcat from '../../assets/images/Vectorcat.png';
+import imgcrown from '../../assets/images/imgcrown.png';
+import imgcoin from '../../assets/images/coin.png';
+import imgclose from '../../assets/images/img-close.png';
+import imghome from '../../assets/images/img_home.png';
+import imgmenu from '../../assets/images/img_menu.png';
+import imgcart from '../../assets/images/img_cart.png';
+import imgaddpet from '../../assets/images/img_add_pet.png';
+import imgpet1 from '../../assets/images/img_pet_1.png';
+import imgpet2 from '../../assets/images/img_pet_2.png';
+import imgpet3 from '../../assets/images/img_pet_3.png';
+import imgpet4 from '../../assets/images/img_pet_4.png';
+const color_text_black = "#221E1B";
+const color_text_yellow = "#F3AE29";
+const color_background = "#f3f4df";
+const color_background_white = "#FFFFFF";
+const color_background_blue_3 = "#398AFF";
+const color_background_blue_1 = "#81D9FF";
+const color_background_blue_2 = "#D0EFFF";
+const color_background_green_1 = "#7DF4A8";
+const color_background_green_2 = "#008000";
+const color_background_gray = "rgba(0, 0, 0, 0.21)";
+const color_background_gray_1 = "#edede9";
+const color_background_red = "#e01e37";
+const color1 = "#E8F7FE";
+const color2 = "#EAF9F2";
+const color3 = "#F0F3FE";
+const color4 = "#FAF2EB";
+const color5 = "#71A8B6";
 const HomeSreen = ({ navigation }) => {
     const [isLoading, setLoading] = useState(true);
     const { userInfo, logout } = useContext(AuthContext);
     const [data, setData] = useState([]);
-
-
-    const imgs = [medal1, medal2, medal3, medal4];
-    let rank = 0;
-    let nubicon = 0;
+    const imgs = [imgaddpet, imgpet1, imgpet2, imgpet3, imgpet4];
+    const colors = [color1, color2, color3, color4, color5];
+    const items = [img1, img2, img3, img4, img5, img6, img7];
+    let nubimg = 0;
+    let nubcolor = 0;
     const getLessons = async () => {
         try {
             const response = await fetch(`${BASE_URL}/lession/all`);
@@ -42,305 +84,181 @@ const HomeSreen = ({ navigation }) => {
     return (
         <View style={styles.container}>
             <View style={styles.header}>
-                <TouchableOpacity onPress={() => navigation.navigate('Menu')}>
-                    <Image style={styles.menu} source={require('../../assets/images/menu.png')} />
+                <TouchableOpacity style={styles.header_left} onPress={() => navigation.navigate("Menu")}>
+                    <Image style={styles.header_left_img} source={imgback} />
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
-                    <Image style={styles.menu} source={require('../../assets/images/info.png')} />
+                <TouchableOpacity style={styles.header_right} onPress={() => navigation.navigate("Menu")}>
+                    <Image style={styles.header_right_img} source={imgcart} />
                 </TouchableOpacity>
             </View>
-            <Text style={styles.infoUser}> Chào {userInfo.name},</Text>
-            <Text style={styles.desUser}>Có bài học mới cho bạn nè!</Text>
             <View style={styles.banner}>
-                <Image style={styles.imgbanner} source={require('../../assets/images/item_home_banner.png')} />
-                <View style={styles.introBanner}>
-                    <Text style={styles.desBanner}>Học...Học nữa...Học mãi...</Text>
-                    <TouchableOpacity style={styles.btnBanner} onPress={() => navigation.navigate('Lesson')}>
-                        <Text style={styles.textBanner}>Xem thêm</Text>
+                <View style={styles.banner_header}>
+                    <Text style={styles.banner_header_text_1}>
+                        Thu cưng
+                    </Text>
+                    <Text style={styles.banner_header_text_2}>
+                        Khám phá
+                    </Text>
+                </View>
+                <View style={styles.banner_list}>
+                    <TouchableOpacity style={styles.content_bottom_item}>
+                        <Image style={styles.content_bottom_item_img} source={imgs[0]} ></Image>
+                        <Text style={styles.content_bottom_item_text}>Thêm</Text>
                     </TouchableOpacity>
-                </View>
-
+                    <TouchableOpacity style={styles.content_bottom_item}>
+                        <Image style={styles.content_bottom_item_img} source={imgs[1]} ></Image>
+                        <Text style={styles.content_bottom_item_text}>Gogo</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.content_bottom_item}>
+                        <Image style={styles.content_bottom_item_img} source={imgs[2]} ></Image>
+                        <Text style={styles.content_bottom_item_text}>Meoo</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.content_bottom_item}>
+                        <Image style={styles.content_bottom_item_img} source={imgs[3]} ></Image>
+                        <Text style={styles.content_bottom_item_text}>Ooo</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.content_bottom_item}>
+                        <Image style={styles.content_bottom_item_img} source={imgs[4]} ></Image>
+                        <Text style={styles.content_bottom_item_text}>Chis</Text>
+                    </TouchableOpacity>
+                </View>    
             </View>
-            <View style={styles.context}>
-                <View style={styles.listbtn}>
-                    <View style={styles.itembtn}>
-                        <Text style={styles.itemtext} >Bài học</Text>
-                    </View>
-                    <View >
-                        <TouchableOpacity style={styles.itembtn1} onPress={() => navigation.navigate('Listgame')}>
-                            <Text style={styles.itemtext} >Trò chơi</Text>
-                        </TouchableOpacity>
-                    </View>
-                    <View>
-                        <TouchableOpacity style={styles.itembtn2} onPress={() => navigation.navigate('Rank')}>
-                            <Text style={styles.itemtext} >Xếp hạng</Text>
-                        </TouchableOpacity>
-                    </View>
-
+            {isLoading ? (
+             <ActivityIndicator />
+             ) : (
+            <View style={styles.body}>
+                <View style={styles.banner_header}>
+                    <Text style={styles.banner_header_text_1}>
+                        Chủ đề
+                    </Text>
+                    <Text style={styles.banner_header_text_2}>
+                        Khám phá
+                    </Text>
                 </View>
-
-                {isLoading ? (
-                    <ActivityIndicator />
-                ) : (
-
-                    <View style={styles.listlesstion}>
-                        <FlatList contentContainerStyle={styles.list}
-                            data={data}
-                            keyExtractor={({ id }) => id}
-                            renderItem={({ item }) => (
-                                <View style={styles.itemlesson}>
-                                    <View style={styles.content}>
-                                        <Image style={styles.item_img} source={imgs[nubicon++]}></Image>
-                                        <Text style={styles.name}>{item.name}</Text>
-                                        {/* <Text style={styles.description}>{item.description}</Text> */}
-                                    </View>
-                                    <View style={styles.btn_bottom}>
-                                        <TouchableOpacity style={styles.btn} onPress={() => this._handleSubmit(item.id, item.name)} >
-                                            <Text style={styles.textbtn}>Chọn</Text>
-                                        </TouchableOpacity>
-
-                                    </View>
-
-                                </View>
-                            )}
-                        />
-
-                    </View>
+                <FlatList
+                    contentContainerStyle={styles.body_list}
+                    data={data}
+                    keyExtractor={({ id }) => id}
+                    renderItem={({ item }) => (
+                    <TouchableOpacity
+                        onPress={() => this._handleSubmit(item.id,item.name)}
+                        style={{
+                        width: 170,
+                        height: 170,
+                        backgroundColor: colors[nubcolor++],
+                        borderRadius: 20,
+                        justifyContent:'center',
+                        alignItems:'center',
+                        gap: 5
+                    }}>
+                        <Image style={styles.body_item_img} source={items[nubimg++]}></Image>
+                        <Text style={styles.body_item_text}>{item.name}</Text>
+                    </TouchableOpacity>
                 )}
+                />
             </View>
+        )}  
         </View>
     )
 };
 
 const styles = StyleSheet.create({
-    btn: {
-        backgroundColor: 'white',
-        width: 70,
-        height: 40,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 12,
-        marginTop: 25,
-        marginRight: -5
+    container:{
+        padding:15,
+        paddingTop: 25,
+        height:'100%',
+        backgroundColor:color_background
     },
-
-    description: {
-        fontSize: 16,
-        color: 'white'
+    header:{
+        flexDirection:'row',
+        width:'100%',
+        justifyContent:'space-between'
     },
-    container: {
-        flex: 1,
-        width: '100%',
-        height: '100%',
-        backgroundColor: '#fff',
-        display: 'flex',
-        alignItems: 'center'
+    header_right:{
+        width: 32,
+        height: 32
     },
-    header: {
+    header_left:{
+        width: 32,
+        height: 32
+    },
+    header_right_img:{
+        width: 32,
+        height: 32
+    },
+    header_left_img:{
+        width: 32,
+        height: 32
+    },
+    banner:{
+        backgroundColor:color_background_white,
+        padding: 15,
+        borderRadius: 22,
         marginTop: 10,
-        paddingLeft: 20,
-        paddingRight: 20,
+    },
+    banner_header:{
+        flexDirection:'row',
         width: '100%',
-        height: 70,
-        flexDirection: 'row',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between'
+        justifyContent:'space-between',
+        paddingBottom: 10,
+        paddingTop: 5,
+        alignItems:'flex-end'
     },
-    infoUser: {
-        fontSize: 20,
-        width: 400,
-        fontWeight: '800',
-        left: 25
+    banner_header_text_1:{
+        fontSize: 17,
+        fontWeight:'500',
+        color:color_text_black
     },
-    desUser: {
-        fontSize: 16,
-        fontWeight: '400',
-        marginRight: '36%'
-    },
-    banner: {
-        width: '75%',
-        height: '15%',
-        display: 'flex',
-        alignItems: 'center',
-
-        backgroundColor: '#C0EDFC',
-        borderRadius: 12,
-        marginTop: 40,
-        flexDirection: 'row',
-        marginBottom: 20
-    },
-    introBanner: {
-        marginRight: '40%',
-
-    },
-    desBanner: {
+    banner_header_text_2:{
         fontSize: 14,
-
+        fontWeight:'500',
+        color:color_background_gray
     },
-
-    btnBanner: {
-        display: 'flex',
-        marginTop: 20,
-        marginLeft: 100,
-        width: 80,
-        height: 30,
-        backgroundColor: '#fff',
-        borderRadius: 8,
-        justifyContent: 'center',
-        alignItems: 'center'
-
-
+    banner_list:{
+        flexDirection:'row',
+        justifyContent:'space-between'
     },
-    context: {
-        width: '100%',
-        height: '55%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'start'
+    content_bottom_item:{
+        alignItems:'center',
+        justifyContent:'space-between',
     },
-    menu: {
-        width: 40,
-        height: 40,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
+    content_bottom_item_img:{
+ 
     },
-    imgbanner: {
-        width: 150,
-        height: 150,
-        objectFit: 'contain',
-        display: 'flex',
-        marginLeft: '-10%',
-        marginBottom: 30
-
+    content_bottom_item_text:{
+        color:color_text_black,
+        fontWeight:'500'
     },
-    list: {
-        width: 320,
-        display: "flex",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        gap: 10,
-        justifyContent: 'space-between',
+    body:{
+        padding: 15,
+        marginTop: 15,
+        backgroundColor:color_background_white,
+        borderRadius: 22,
     },
-    listbtn:
-    {
-        width: 450,
-        height: 50,
-        gap: 10,
-        flexDirection: 'row',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 20
+    body_list:{
+        flexDirection:'row',
+        flexWrap: 'wrap',
+        gap: 10
     },
-    itembtn:
-    {
+    body_item:{
+        width: 170,
+        height: 170,
+        backgroundColor:'red',
+        borderRadius: 20,
+        justifyContent:'center',
+        alignItems:'center',
+        gap: 5
+    },
+    body_item_text:{
         width: 100,
-        height: 40,
-        backgroundColor: '#62C7F3',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 8
+        textAlign: 'center',
+        lineHeight: 18,
+        color:color_text_black,
+        fontWeight: '400'
     },
-    itembtn1: {
-        width: 100,
-        height: 40,
-        backgroundColor: '#B8E1FF',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 8
-    },
-    itembtn2: {
-        width: 100,
-        height: 40,
-        backgroundColor: '#CED1E6',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 8
-    },
-    itemtext: {
-        fontSize: 16,
-
-    },
-    listlesstion:
-    {
-        paddingHorizontal: 20,
-        width: 360,
-        paddingVertical: 15,
-        backgroundColor: "#fff",
-        borderRadius: 15,
-        height: 300,
-        justifyContent: 'space-between',
-        top: 10
-
-
-    },
-
-    itemlesson:
-    {
-
-        borderRadius: 16,
-        height: 120,
-        width: 140,
-
-        marginBottom: 20,
-        backgroundColor: '#fff',
-        padding: 20,
-        borderWidth: 1,
-        borderStyle: 'solid',
-        borderColor: '#62C7F3',
-        marginRight: 10
-
-    },
-    content: {
-        gap: 5,
-        width: 120,
-        height: 40,
-        left: -13
-
-
-    },
-    item_img: {
-        width: 30,
-        height: 30,
-        top: -20,
-
-
-
-
-    },
-    name: {
-        color: '#000',
-        fontWeight: '600',
-        top: -25,
-        height: 65
-
-    },
-    description: {
-        color: '#000',
-        backgroundColor: 'red',
-        height: 40
-    },
-    btn_bottom: {
-
-        marginTop: -35,
-        left: 48
-    },
-    btn: {
+    body_item_img:{
         width: 60,
-        height: 25,
-        backgroundColor: '#62C7F3',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 4,
-        marginTop: 60,
-        marginRight: 20
+        height: 60
     }
-
 })
 export default HomeSreen;
